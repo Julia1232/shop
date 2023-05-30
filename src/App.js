879fd748a26668fc7196bab5825ff0a9dd898a61
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2023-05-30 12:30:24 +0300

    six

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+import Header from './components/HeaderPage';
 import Categories from './components/Categories';
 import React from 'react';
 import Footer from './components/Footer';
